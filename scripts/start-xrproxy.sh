fbf48b6cd752cc6a8143ded6777042af81ea8018
luusluus <lucienmartijn@gmail.com>
2020-12-25 19:21:53 +0100

    update env

--- scripts/start-xrproxy.sh
+++ scripts/start-xrproxy.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-cat > /opt/uwsgi/conf/uwsgi.ini << EOL
-[uwsgi]
-processes = 4
-threads = 4
-buffer-size = 32768
-set-ph = SERVICENODE_PRIVKEY=${SN_KEY}
-set-ph = BLOCKNET_CHAIN=mainnet
-set-ph = RPC_BLOCK_HOSTIP=snode
-set-ph = RPC_BLOCK_PORT=41414
-set-ph = RPC_BLOCK_USER=${RPC_USER}
-set-ph = RPC_BLOCK_PASS=${RPC_PASSWORD}
-set-ph = RPC_BLOCK_VER=2.0
-EOL
-
 # ensure supervisord runs at pid1
 exec supervisord -c /etc/supervisord.conf
