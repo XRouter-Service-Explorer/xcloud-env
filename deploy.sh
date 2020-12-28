ddd3d27671e6cc17b66ac75cc3c18817260f40b7
luusluus <lucienmartijn@gmail.com>
2020-12-28 19:58:09 +0100

    fix references

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ else
   echo "Not installing Docker..."
 fi
 
-read -p 'Your Public IP Address: ' PUBLIC_IP
+read -p 'Your Domain name: ' SERVER_NAME
 
 # GETH EXTERNAL
 if [ "$1" = "gethexternal" ]; then
@@ -50,11 +50,5 @@ read -p 'RPC Username: ' RPC_USER
 read -sp 'RPC Password: ' RPC_PASSWORD
 echo
 
-if [ "$1" = "dev" ]; then
-  echo "Development mode"
-  PUBLIC_IP=$PUBLIC_IP SN_NAME=$SN_NAME SN_KEY=$SN_KEY SN_ADDRESS=$SN_ADDRESS RPC_USER=$RPC_USER RPC_PASSWORD=$RPC_PASSWORD docker-compose -f "docker-compose-dev.yml" up -d --build
-elif [ "$1" = "gethexternal" ]; then
-  PUBLIC_IP=$PUBLIC_IP SN_NAME=$SN_NAME SN_KEY=$SN_KEY SN_ADDRESS=$SN_ADDRESS RPC_USER=$RPC_USER RPC_PASSWORD=$RPC_PASSWORD GETH_HOST=$GETH_HOST docker-compose -f "docker-compose-geth-external.yml" up -d --build
-else
-  PUBLIC_IP=$PUBLIC_IP SN_NAME=$SN_NAME SN_KEY=$SN_KEY SN_ADDRESS=$SN_ADDRESS RPC_USER=$RPC_USER RPC_PASSWORD=$RPC_PASSWORD docker-compose -f "docker-compose.yml" up -d --build
-fi
+
+SERVER_NAME=$SERVER_NAME SN_NAME=$SN_NAME SN_KEY=$SN_KEY SN_ADDRESS=$SN_ADDRESS RPC_USER=$RPC_USER RPC_PASSWORD=$RPC_PASSWORD docker-compose -f "docker-compose.yml" up -d --build
